72c67a78e4935a4e81335ba51f333eece862976a
Timur <tagirovta22@st.ithub.ru>
2025-02-03 18:06:31 +0500

    .

--- kohonen_network.py
+++ kohonen_network.py
@@ -21,8 +21,14 @@ input_weights = [
 output_weights = [[1, -1, -0.5]]
 
 # Комбинированные вводы для скрытых нейронов
-f_combined = data_sensor[0] * input_weights[0][0] + data_sensor[0] * input_weights[1][0] + input_weights[0][2]
-s_combined = data_sensor[1] * input_weights[1][1] + data_sensor[1] * input_weights[0][1] + input_weights[1][2]
+f_combined = (data_sensor[0] * input_weights[0][0] +
+              data_sensor[0] * input_weights[1][0] +
+              input_weights[0][2])
+
+s_combined = (data_sensor[1] * input_weights[1][1] +
+              data_sensor[1] * input_weights[0][1] +
+              input_weights[1][2])
+
 hidden_combined = [f_combined, s_combined]
 print(f'Комбинированные вводы для скрытых нейронов: {hidden_combined}')
 
@@ -31,7 +37,10 @@ activity_neuron = [sign_function(f_combined), sign_function(s_combined)]
 print(f'Активности нейронов скрытого слоя: {activity_neuron}')
 
 # Комбинированный ввод выходного нейрона
-output_combined = activity_neuron[0] * output_weights[0][0] + activity_neuron[1] * output_weights[0][1] + output_weights[0][2]
+output_combined = (activity_neuron[0] * output_weights[0][0] +
+                   activity_neuron[1] * output_weights[0][1] +
+                   output_weights[0][2])
+
 print(f'Комбинированный ввод входного нейрона: {output_combined}')
 
 # Активность выходного нейрона
