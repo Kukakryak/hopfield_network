4bf78282dc3193eaf78559f5536961e765c86f9d
Timur <tagirovta22@st.ithub.ru>
2025-02-13 14:50:57 +0500

    .

--- kohonen_network.py
+++ kohonen_network.py
@@ -1,48 +1,68 @@
+from math import e as euler
 # Входные данные
-data_sensor = [1, 1]
+data_sensor = [0.6, 0.7]
+
+# Ожидаемый выход сети
+expected_output = 0.9
+
+# Норма обучения
+training_rate = 0.3
+
+# Момент
+y = 0.3
 
 # Элемент смещения
 bias1 = 1
 bias2 = 1
 
-# Функция активации (прыжковая)
-def sign_function(neuron: float):
-    if neuron > 0.0: neuron = 1.0
-    if neuron < 0.0: neuron = 0.0
-    return neuron
+# Сигмоидная функция активации
+def sign_function(combined_input):
+    return 1 / (1 + pow(euler, -combined_input))
 
 # Весовые коэффициенты от входного слоя к первому скрытому слою ( номер столбца - номер входного нейрона, номер строки - номер скрытого нейрона )
-input_weights = [
-  [  -1,  -1,  1.5 ],
-  [  -1,  -1,  0.5 ]
+input_weights1 = [
+  [-1,   1 ],
+  [2.5, 0.4],
+  [ 1, -1.5]
 ]
 
-# Весовые коэффициенты от первого скрытого слоя к выходному
-output_weights = [[1, -1, -0.5]]
+# Весовые коэффициенты от первого скрытого слоя ко второму скрытому слою
+input_weights2 = [
+    [2.2, -1.4,  0.56],
+    [0.34, 1.05, 3.10]
+]
 
-# Комбинированные вводы для скрытых нейронов
-f_combined = (data_sensor[0] * input_weights[0][0] +
-              data_sensor[1] * input_weights[1][0] +
-              input_weights[0][2])
+# Весовые коэффициенты от второго скрытого слоя к выходному
+output_weights = [[0.75, -0.22]]
 
-s_combined = (data_sensor[1] * input_weights[1][1] +
-              data_sensor[0] * input_weights[0][1] +
-              input_weights[1][2])
+# Комбинированные вводы для нейронов первого скрытого слоя
+hidden_combined1 = [data_sensor[0] * input_weights1[0][0] + data_sensor[1] * input_weights1[0][1],
+                    data_sensor[0] * input_weights1[1][0] + data_sensor[1] * input_weights1[1][1],
+                    data_sensor[0] * input_weights1[2][0] + data_sensor[1] * input_weights1[2][1]]
+hidden_combined1 = list(map(lambda x: round(x, 5) , hidden_combined1))
+print("Комбинированные вводы для нейронов первого скрытого слоя:", hidden_combined1)
 
-hidden_combined = [f_combined, s_combined]
-print(f'Комбинированные вводы для скрытых нейронов: {hidden_combined}')
+# Активности нейронов первого скрытого слоя
+activity_neuron1 = list(map(lambda x: round(sign_function(x), 5), hidden_combined1))
+print("Активности нейронов первого скрытого слоя:", activity_neuron1)
 
-# Активности нейронов скрытого слоя
-activity_neuron = [sign_function(f_combined), sign_function(s_combined)]
-print(f'Активности нейронов скрытого слоя: {activity_neuron}')
+# Комбинированные вводы для нейронов второго скрытого слоя
+hidden_combined2 = [activity_neuron1[0] * input_weights2[0][0] +
+                    activity_neuron1[1] * input_weights2[0][1] +
+                    activity_neuron1[2] * input_weights2[0][2],
+
+                    activity_neuron1[0] * input_weights2[1][0] +
+                    activity_neuron1[1] * input_weights2[1][1] +
+                    activity_neuron1[2] * input_weights2[1][2]  ]
+hidden_combined2 = list(map(lambda x: round(x, 5) , hidden_combined2))
+print("Комбинированные вводы для нейронов второго скрытого слоя:", hidden_combined2)
+
+# Активности нейронов второго скрытого слоя
+activity_neuron2 = list(map(lambda x: round(sign_function(x), 5), hidden_combined2))
+print("Активности нейронов второго скрытого слоя:", activity_neuron2)
 
 # Комбинированный ввод выходного нейрона
-output_combined = (activity_neuron[0] * output_weights[0][0] +
-                   activity_neuron[1] * output_weights[0][1] +
-                   output_weights[0][2])
-
-print(f'Комбинированный ввод входного нейрона: {output_combined}')
+output_combined = None
 
 # Активность выходного нейрона
-activity_output_neuron = sign_function(output_combined)
-print(f'Активность выходного нейрона: {activity_output_neuron}')
+activity_output_neuron = None
