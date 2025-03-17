8d11dbcb1eb0061c6506f18a4b60ceab8fdcdc98
Timur <tagirovta22@st.ithub.ru>
2025-03-17 20:02:28 +0500

    .

--- convolutional.py
+++ convolutional.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
-import math
 
 def show_matrix(matr: list[list[int]]):
-    plt.figure(figsize=(len(matr), len(matr[0])))
+    plt.figure(figsize=(len(matr[0]), len(matr)))
     plt.pcolormesh(matr, edgecolors='black', cmap='gist_yarg')
+    plt.gca().invert_yaxis()
     plt.show()
 
 def print_matrix(matr: list[list[int]]):
@@ -16,10 +16,9 @@ def print_matrix(matr: list[list[int]]):
 def matrix_multiply(matrix: list[list[int]], scnd_matrix = None, vector = None):
     result = 0
     vector = matrix_to_vector(matrix)
-    v_iter = iter(vector)
-    for i in range(len(scnd_matrix)):
-        for j in range(len(scnd_matrix[i])):
-            result += scnd_matrix[i][j]*next(v_iter)
+    scnd_vector = matrix_to_vector(scnd_matrix)
+    for x, y in zip(vector, scnd_vector):
+        result+=(x*y)
     return result/9
 
 def matrix_to_vector(matrix: list[list[int]]):
@@ -33,31 +32,60 @@ def matrix_sum(matrix: list[list[int]], scnd_matrix: list[list[int]]):
     result = []
     for row, scnd_row in zip(matrix, scnd_matrix):
         temp = []
-        first_iter = iter(row)
-        scnd_iter = iter(scnd_row)
-        for x, y in zip(first_iter, scnd_iter):
+        for x, y in zip(row, scnd_row):
             temp.append(x+y)
         result.append(temp)
     return result
 
+def sum_layers(layers: list[list[list[int]]]):
+    summary = layers[0]
+    for matr in layers:
+        if matr != summary:
+            summary = matrix_sum(summary, matr)
+    return summary
+
 def convolution(a: list[list[int]], b: list[list[int]]):
     result = []
-    for j in range(layer_length):
+    for i in range(layer_length):
         s = []
-        for k in range(layer_length):
+        for j in range(layer_width):
             try:
                 t = []
                 # один цикл == один кусок матрицы
-                for i in range(kernel_length):
-                    t.append(a[j+i][k:k+kernel_length])
+                for k in range(kernel_length):
+                    t.append(a[i+k][j:j+kernel_length])
                 s.append(matrix_multiply(matrix=t, scnd_matrix=b))
             except IndexError:
                 break
         result.append(s)
     return result
 
+# Итеративность поломана, так что больше одной итерации не сработает
+def reduce_image_size(image: list[list[int]], kernels: list[list[list[int]]], iters: int = 1):
+    global layer_length, layer_width
+    for i in range(iters):
+        layers = [convolution(image, kern) for kern in kernels]
+        for j in range(kernels_amount):
+            try:
+                image = sum_layers(layers)
+            except IndexError:
+                layer_length = len(image)
+                layer_width = len(image[0])
+                break
+    return image
 
 
+def extract_object(image: list[list[int]]):
+    def transposition(matr: list[list[int]]):
+        result = []
+        for i in range(len(matr[0])):
+            temp = []
+            for j in range(len(matr)):
+                temp.append(matr[j][i])
+            if sum(temp) != -len(temp):
+                result.append(temp)
+        return result
+    return transposition(transposition(image))
 image_length = 9
 image_width = 9
 kernel_length = 3
@@ -65,15 +93,29 @@ kernel_width = 3
 kernels_amount = 3
 layer_length = 7
 layer_width = 7
-img = [[1, -1, -1, -1, -1, -1, -1, -1, 1],
-         [-1, 1, -1, -1, -1, -1, -1, 1, -1],
-         [-1, -1, 1, -1, -1, -1, 1, -1, -1],
-         [-1, -1, -1, 1, -1, 1, -1, -1, -1],
-         [-1, -1, -1, -1, 1, -1, -1, -1, -1],
-         [-1, -1, -1, 1, -1, 1, -1, -1, -1],
-         [-1, -1, 1, -1, -1, -1, 1, -1, -1],
-         [-1, 1, -1, -1, -1, -1, -1, 1, -1],
-         [1, -1, -1, -1, -1, -1, -1, -1, 1]]
+img = [
+  [  1, -1, -1, -1, -1, -1, -1, -1,  1 ],
+  [ -1,  1, -1, -1, -1, -1, -1,  1, -1 ],
+  [ -1, -1,  1, -1, -1, -1,  1, -1, -1 ],
+  [ -1, -1, -1,  1, -1,  1, -1, -1, -1 ],
+  [ -1, -1, -1, -1,  1, -1, -1, -1, -1 ],
+  [ -1, -1, -1,  1, -1,  1, -1, -1, -1 ],
+  [ -1, -1,  1, -1, -1, -1,  1, -1, -1 ],
+  [ -1,  1, -1, -1, -1, -1, -1,  1, -1 ],
+  [  1, -1, -1, -1, -1, -1, -1, -1,  1 ]
+]
+
+img_2 = [
+  [ -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+  [ -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+  [ -1, -1, -1,  1,  1,  1, -1, -1, -1 ],
+  [ -1, -1,  1, -1, -1, -1,  1, -1, -1 ],
+  [ -1, -1,  1, -1,  1, -1,  1, -1, -1 ],
+  [ -1, -1,  1, -1, -1, -1,  1, -1, -1 ],
+  [ -1, -1, -1,  1,  1,  1, -1, -1, -1 ],
+  [ -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+  [ -1, -1, -1, -1, -1, -1, -1, -1, -1 ]
+]
 
 convolution_kernels = [
 [
@@ -92,19 +134,12 @@ convolution_kernels = [
     [1,-1,-1],
 ]
 ]
-con1 = convolution(img, convolution_kernels[0])
-con2 = convolution(img, convolution_kernels[1])
-con3 = convolution(img, convolution_kernels[2])
-convolution_layers = [con1, con2, con3]
-con_sum = matrix_sum(con3, matrix_sum(con1, con2))
-for con in convolution_layers:
-    print_matrix(con)
-print_matrix(con_sum)
-show_matrix(img)
-show_matrix(con_sum)
-for j in con_sum:
-    for k in range(len(j)):
-        if j[k] > 1: j[k] = 1
-        else: j[k] = -1
-show_matrix(con_sum)
-print_matrix(con_sum)
+
+def tests():
+    reduced = reduce_image_size(image=img, kernels=convolution_kernels)
+    show_matrix(img)
+    show_matrix(reduced)
+    show_matrix(img_2)
+    show_matrix(extract_object(img_2))
+if __name__ == '__main__':
+    tests()
